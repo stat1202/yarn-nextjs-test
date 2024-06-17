8054844878ff2f88129a00df1d8139096e771178
BIROOM <zoon1031@naver.com>
2024-06-17 16:04:19 +0900

    feat #41 : make issue

--- src/app/YHS/page.tsx
+++ src/app/YHS/page.tsx
@@ -1,7 +1,7 @@
 export default function YHS() {
   return (
     <>
-      <h1>YHS Component</h1>
+      <h1>confilicts weqioehwqoiehowqioewq Component</h1>
       <h2>commit test</h2>
     </>
   );
