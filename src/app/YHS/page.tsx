9156bf63d6079e16de3f911c30e123d57c9132e9
hunsang <gjstkd123@naver.com>
2024-06-17 16:10:29 +0900

    feat #47 : conflict
    conflict

--- src/app/YHS/page.tsx
+++ src/app/YHS/page.tsx
@@ -1,7 +1,9 @@
 export default function YHS() {
   return (
     <>
-      <h1>confilicts weqioehwqoiehowqioewq Component</h1>
+      <h1>
+        confalskdmlaksdlaksdgogogogogogoilicts weqioehwqoiehowqioewq Component
+      </h1>
       <h2>commit test</h2>
       <h2>conflict test</h2>
       <h2>conflict test</h2>
