2b794a703f6b98531addde0c6a923bf01fea5fde
BIROOM <zoon1031@naver.com>
2024-06-17 16:11:34 +0900

    feat #48 : conflicts

--- src/app/YHS/page.tsx
+++ src/app/YHS/page.tsx
@@ -2,7 +2,8 @@ export default function YHS() {
   return (
     <>
       <h1>
-        confalskdmlaksdlaksdgogogogogogoilicts weqioehwqoiehowqioewq Component
+        updated componetneqwpjehowiqheiowqheiowqshioe fioncikjewqblqwe
+        weqioehwqoiehowqioewq Component
       </h1>
       <h2>commit test</h2>
       <h2>conflict test</h2>
